086cfe008cb4a0f20c2a6f5db4f4af16305738fd
robbie <rob.643@hotmail.co.uk>
2025-09-25 07:58:14 +0100

    uses css variables to reduce repetition of height width

--- lessons/front_end/mini_projects/image_carousel/main.js
+++ lessons/front_end/mini_projects/image_carousel/main.js
@@ -1,7 +1,7 @@
 const carouselControls = document.getElementById('carousel-controls');
 const imagesContainer = document.getElementById('images-container');
 let carouselIndex = 0;
-
+let imageWidth=700;
 let numImages =imagesContainer.children.length;
 
 function increment() {
@@ -15,12 +15,12 @@ carouselControls.addEventListener('click', (e) => {
     
     if (e.target.id == 'carousel-prev'){
         decrement();
-        let translationPixels = carouselIndex*500; 
+        let translationPixels = carouselIndex*imageWidth; 
         imagesContainer.style.transform = `translateX(-${translationPixels}px)`
 
     } else if (e.target.id == 'carousel-next') {
         increment();
-        let translationPixels = carouselIndex*500; 
+        let translationPixels = carouselIndex*imageWidth; 
         imagesContainer.style.transform = `translateX(-${translationPixels}px)`
     }
 })
